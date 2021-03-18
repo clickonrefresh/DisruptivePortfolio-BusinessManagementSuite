515f4bc7c4337d31faefc0fa70057c21e7dc6a61
clickonrefresh <72121107+clickonrefresh@users.noreply.github.com>
2021-03-18 03:56:22 +0200

    Create install.sh

---
+++ VagrantHost/vms/admin/install.sh
@@ -0,0 +1,39 @@
+     sudo apt-get update
+     sudo apt-get upgrade -y
+     sudo apt-get install zip -y
+     sudo apt-get install fail2ban -y
+     sudo apt-get install curl -y
+     sudo apt-get install wget -y
+     
+     sudo apt install apt-transport-https ca-certificates curl software-properties-common -y
+     sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+     sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+     sudo apt update
+     sudo apt install docker-ce -y
+     sudo curl -L "https://github.com/docker/compose/releases/download/1.27.4/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+     sudo chmod +x /usr/local/bin/docker-compose
+     sudo apt-get update
+     sudo apt-get upgrade -y
+     
+     sudo usermod -aG sudo vagrant
+     
+     sleep 20s
+     cd /var
+     cd www
+     cd nginxproxymanager
+     sudo docker-compose up -d
+     cd ..
+     
+     sleep 20s
+     sudo docker volume create portainer_data
+     sudo docker run -d -p 8023:8000 -p 9023:9000 --name=portainer --restart=always -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer-ce
+     
+     sudo apt install cockpit -y
+     
+     cd /opt
+     sudo mkdir noip && cd noip
+     sudo wget https://github.com/Neill-H/Automate-Noip-DUC/archive/main.zip
+     sudo unzip main.zip
+     sudo init 6
+     
+     
